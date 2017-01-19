53eb43d62a0edb1654441475b6a94b6231aebd5c
周宝川 <zhoubaochuan@baidu.com>
2017-01-19 11:05:23 +0800

    exc

---
+++ Coding.java
@@ -0,0 +1,16 @@
+/**
+* int型变量和char型变量的转换，以此来获取Unicode编码和数字对应的字符。
+*/
+public class Coding{
+	public static void main(String[] args){
+		int a = 'a';
+		int k = 'k';
+		int L = 'L';
+
+		char u211 = 211;
+		char u79 = 79;
+		char u36 = 36;
+		
+		System.out.println(a + "\t" + k + "\t" + L + "\t" + u211 + "\t" + u79 + "\t" + u36);
+	}
+}
