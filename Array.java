481e7d1b01becc1ddb575cb5cf3837977e30433d
周宝川 <zhoubaochuan@baidu.com>
2017-01-20 15:20:21 +0800

    var_dump

---
+++ Array.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+/**
+*  打印数组
+*/
+public class Array{
+	public static void main(String[] args){
+		String[] str = new String[]{"hello", "Abc", "OK"};
+		
+		System.out.println(Arrays.asList(str));
+	}
+}
